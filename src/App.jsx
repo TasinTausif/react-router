0a3d67542a55e14fb1698dc9c7244fae4e133b56
TasinTausif <tasintausif@gmail.com>
2025-11-12 21:23:15 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {Header, Footer} from './components'
+function App() {
+
+  return (
+    <>
+      <Header/>
+        
+      <Footer/>
+    </>
+  )
+}
+
+export default App
